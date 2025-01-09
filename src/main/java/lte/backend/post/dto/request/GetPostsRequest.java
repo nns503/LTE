91a6b9882ee83303abd1c251c1fcc256cbfa1b5f
nns503 <73779581+nns503@users.noreply.github.com>
2025-01-09 14:24:53 +0900

    docs: README.md 생성

--- src/main/java/lte/backend/post/dto/request/GetPostsRequest.java
+++ src/main/java/lte/backend/post/dto/request/GetPostsRequest.java
@@ -6,11 +6,11 @@ import lte.backend.post.domain.PostSortBy;
 
 @Schema(description = "게시글 검색 요청 데이터")
 public record GetPostsRequest(
-        @Schema(description = "검색 내용 조건", example = "TITLE")
+        @Schema(description = "검색 내용 조건", example = "TITLE", nullable = true)
         PostSearchBy searchBy,
-        @Schema(description = "검색 내용", example = "나테스트")
+        @Schema(description = "검색 내용", example = "나테스트", nullable = true)
         String searchValue,
-        @Schema(description = "정렬 조건", example = "DATE")
+        @Schema(description = "정렬 조건", example = "DATE", nullable = true)
         PostSortBy sortBy
 ) {
 }
