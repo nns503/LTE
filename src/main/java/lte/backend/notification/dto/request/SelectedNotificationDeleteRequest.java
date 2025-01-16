770f18af71229bbcbab9c0c57d62fc8a39e30aff
nns503 <73779581+nns503@users.noreply.github.com>
2025-01-16 23:29:23 +0900

    fix: Swagger DTO 문서 누락 수정

--- src/main/java/lte/backend/notification/dto/request/SelectedNotificationDeleteRequest.java
+++ src/main/java/lte/backend/notification/dto/request/SelectedNotificationDeleteRequest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 @Schema(description = "선택 알림 삭제 요청 데이터")
 public record SelectedNotificationDeleteRequest(
+        @Schema(description = "선택된 알림 삭제 식별자 리스트", requiredMode = Schema.RequiredMode.REQUIRED)
         List<Long> ids
 ) {
 }
--- src/main/java/lte/backend/notification/dto/response/GetNotificationResponse.java
+++ src/main/java/lte/backend/notification/dto/response/GetNotificationResponse.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Schema(description = "알림 목록 조회 응답 데이터")
 public record GetNotificationResponse(
+        @Schema(description = "알림 목록", requiredMode = Schema.RequiredMode.REQUIRED)
         List<GetNotificationDTO> notifications
 ) {
     public static GetNotificationResponse from(List<Notification> notifications) {
